b0454de41c8c10823c1d57744d5aef891ca4f22f
Carlos Alves <carlosalves369m@hotmail.com>
2023-05-16 16:45:21 -0300

    add redis config

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,14 @@ import swaggerUI from 'swagger-ui-express'
 import swaggerDocsV1 from './docs/v1/swagger.json'
 require("dotenv").config()
 
+import { client } from './cache/redis.config'
+
 // controllers
 import '@controllers/UsersController'
 
 // routes
 const v1Router = require('./routes/v1')
+const productsRoutes = require('./routes/v1/products.routes')
 
 import JsonResponse from './concerns/response'
 
@@ -21,6 +24,9 @@ app
     .use("/api/v1/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocsV1 ))
     .use('/api/v1/workouts', v1Router)
 
+    // PRODUCTS
+    .use('/api/v1/products', productsRoutes)
+
     .use((req, res) => {
         const pathname = req.originalUrl
         const result = JsonResponse.response(`Can't found this route: ${pathname}`, false)
@@ -28,9 +34,15 @@ app
         res.status(404).json(result);
     })
 
-    .listen(PORT, () => {
+const startup = async () => {
+    await client.connect()
+
+    app.listen(PORT, () => {
         console.log(`🔥 Server is running in PORT ${PORT} - ${process.env.NODE_ENV}`)
     })
+}
+
+startup()
 
 console.log("🛣️  ROUTES")
 app._router.stack.forEach(print.bind(null, []))
