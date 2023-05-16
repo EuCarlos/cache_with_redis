2049b37b7d6ac247deac9db4b2f56085c2b7e7d0
Carlos Alves <carlosalves369m@hotmail.com>
2023-05-16 17:01:21 -0300

    add comments in source code.

--- src/cache/redis.config.ts
+++ src/cache/redis.config.ts
@@ -1,3 +1,13 @@
 import { createClient } from 'redis'
 
 export const client = createClient();
+
+/**
+ * The above code connects to localhost on port 6379. To connect to a different host or port, 
+ * use a connection string in the format 
+ * `redis[s]://[[username][:password]@][host][:port][/db-number]`
+ * 
+ * createClient({
+ *  url: 'redis://alice:foobared@awesome.redis.server:6380'
+ * });
+ */
